10ea310fe64e7a3ba29ca80563972bdcde9b8b56
Zefirrat <leonidpavlov314@gmail.com>
2020-12-20 12:18:10 +0500

    Star wars seek

---
+++ UnityExperiments/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _horizontalInput = 0;
+    private float _verticalInput = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+   
+
+    }
+
+    void Update()
+    {
+   
+    }
+
+    private void GetPlayerInput()
+    {
+  
+
+    }
+}
